e09d00b442f2161923934bf0cff9e73a8fa49419
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-06-09 05:21:37 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/Blinkingwithoutdelayexp1/Blinkingwithoutdelayexp1.ino
@@ -0,0 +1,31 @@
+const int ledPin = 10; //or pin #13
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    Serial.print("Time: ");
+    Serial.println(currentMillis);
+    Serial.print("Previous Time: ");
+    Serial.println(previousMillis);
+    Serial.print("Time difference: ");
+    Serial.println(currentMillis - previousMillis);
+    if (ledState == LOW){
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState);
+  }
+}
+
