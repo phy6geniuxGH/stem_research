b213a472f27c19e9eb299482edb4bec8fe140944
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-06-11 07:10:47 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/Fadingwithoutdelayexp2/Fadingwithoutdelayexp2.ino
@@ -0,0 +1,33 @@
+const int ledPin = 10; //or pin #13
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 10;
+int brightness = 0;
+int fade = 50;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    Serial.print("Time: ");
+    Serial.println(currentMillis);
+    Serial.print("Previous Time: ");
+    Serial.println(previousMillis);
+    Serial.print("Time difference: ");
+    Serial.println(currentMillis - previousMillis);
+    analogWrite(ledPin, brightness);
+    brightness = brightness + fade;
+    if (brightness <= 0 || brightness >= 255){
+      fade = - fade;
+    }
+  }
+  
+}
+
