fa434aba65f1e128e5f851cad00097d45e5cb2b2
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-06-11 07:09:57 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/DigitalInputPullupExp1/DigitalInputPullupExp1.ino
@@ -0,0 +1,21 @@
+const int pin2 = 2;
+const int pin13 = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pin2, INPUT_PULLUP);
+  pinMode(pin13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorVal = digitalRead(pin2);
+  Serial.println(sensorVal);
+  //pull-up means the pushbutton's logic is inverted.
+  if (sensorVal == HIGH){
+    digitalWrite(pin13, HIGH);
+  } else {
+    digitalWrite(pin13, LOW);
+  }
+}
