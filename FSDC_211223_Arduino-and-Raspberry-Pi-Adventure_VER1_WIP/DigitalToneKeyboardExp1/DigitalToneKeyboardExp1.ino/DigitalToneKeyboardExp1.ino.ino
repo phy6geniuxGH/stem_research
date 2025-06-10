235ae4aa6582744556858c99005b22e1a4e90ab3
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-06-11 07:10:08 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/DigitalToneKeyboardExp1/DigitalToneKeyboardExp1.ino/DigitalToneKeyboardExp1.ino.ino
@@ -0,0 +1,20 @@
+#include "pitches.h"
+//const int threshold = 10;
+int notes[] = {
+  NOTE_A4, NOTE_B4, NOTE_C3
+};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //for(int thisbutton = 0; thisbutton < 3; thisSensor++){
+    int buttonReading = digitalRead(thisbutton);
+    if (buttonReading == HIGH){
+      tone(8, notes[thisbutton], 20);
+    }
+  }
+
