e219dc998c57f9224a6a37c3b52a42a02e332efe
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-06-28 06:50:30 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/PCA9685_calibratingSingleservo_program_/PCA9685_calibratingSingleservo_program_.ino
@@ -0,0 +1,143 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+int rate = 500;
+int angle0 = 140;
+int angle1 = 50;
+int angle2 = 30;
+int angle3 = 30;
+int angle4 = 130;
+int angle5 = 150;
+int angle6 = 140;
+int angle7 = 50;
+int angle8 = 30;
+int angle9 = 30;
+int angle10 = 130;
+int angle11 = 150;
+/*int angle0 = 90;
+int angle1 = 90;
+int angle2 = 90;
+int angle3 = 90;
+int angle4 = 90;
+int angle5 = 90;
+int angle6 = 90;
+int angle7 = 90;
+int angle8 = 90;
+int angle9 = 90;
+int angle10 = 90;
+int angle11 = 90;*/
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  delay(10);
+
+}
+
+void loop() {
+  pwm.setPWM(0, 0, angleToPulse0(angle0));
+  pwm.setPWM(1, 0, angleToPulse1(angle1));
+  pwm.setPWM(2, 0, angleToPulse2(angle2));
+  pwm.setPWM(3, 0, angleToPulse3(angle3));
+  pwm.setPWM(4, 0, angleToPulse4(angle4));
+  pwm.setPWM(5, 0, angleToPulse5(angle5));
+  pwm.setPWM(6, 0, angleToPulse6(angle6));
+  pwm.setPWM(7, 0, angleToPulse7(angle7));
+  pwm.setPWM(8, 0, angleToPulse8(angle8));
+  pwm.setPWM(9, 0, angleToPulse9(angle9));
+  pwm.setPWM(10, 0, angleToPulse10(angle10));
+  pwm.setPWM(11, 0, angleToPulse11(angle11));
+  delay(rate);
+
+}
+
+
+
+int angleToPulse9(int ang) {
+  int pulse9 = map(ang, 0, 180, 180, 500);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse9);
+  return pulse9;
+}
+
+int angleToPulse10(int ang) {
+  int pulse10 = map(ang, 0, 180, 160, 480);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse10);
+  return pulse10;
+}
+
+int angleToPulse11(int ang) {
+  int pulse11 = map(ang, 0, 180, 190, 510);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse11);
+  return pulse11;
+}
+
+int angleToPulse0(int ang) {
+  int pulse0 = map(ang, 0, 180, 160, 480);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse0);
+  return pulse0;
+}
+
+int angleToPulse1(int ang) {
+  int pulse1 = map(ang, 0, 180, 160, 490);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse1);
+  return pulse1;
+}
+
+int angleToPulse2(int ang) {
+  int pulse2 = map(ang, 0, 180, 160, 440);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse2);
+  return pulse2;
+}
+
+int angleToPulse3(int ang) {
+  int pulse3 = map(ang, 0, 180, 185, 500);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse3);
+  return pulse3;
+}
+int angleToPulse4(int ang) {
+  int pulse4 = map(ang, 0, 180, 190, 500);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse4);
+  return pulse4;
+}
+
+int angleToPulse5(int ang) {
+  int pulse5 = map(ang, 0, 180, 190, 490);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse5);
+  return pulse5;
+}
+
+int angleToPulse6(int ang) {
+  int pulse6 = map(ang, 0, 180, 160, 490);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse6);
+  return pulse6;
+}
+
+int angleToPulse7(int ang) {
+  int pulse7 = map(ang, 0, 180, 160, 480);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse7);
+  return pulse7;
+}
+
+int angleToPulse8(int ang) {
+  int pulse8 = map(ang, 0, 180, 175, 510);
+  Serial.print("Angle: "); Serial.print(ang);
+  Serial.print(" pulse: "); Serial.println(pulse8);
+  return pulse8;
+}
