f3f6ccf0ef09f0816782f96ec5a379471eecb64e
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-07-06 00:33:25 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/Sharp_IR_Calibration/Irscalib.cpp/Irscalib.cpp.ino
@@ -0,0 +1,50 @@
+/*
+ * Practice for Library Making
+ */
+
+
+#include "Arduino.h"
+#include "Irscalib.h"
+
+Irscalib::Irscalib(int sensorPin)
+{
+  _sensorPin = sensorPin;
+}
+Irscalib::Irscalib(int sensorValue)
+{
+  _sensorValue = sensorValue;
+}
+Irscalib::Irscalib(int sensorMin)
+{
+  _sensorMin = sensorMin;
+}
+Irscalib::Irscalib(int sensorMax)
+{
+  _sensorMax = sensorMax;
+}
+
+void Irscalib::adjustSensor()
+{
+  Serial.begin(9600);
+  while (millis() < 10000){
+    sensorValue = analogRead(_sensorPin);
+    if(_sensorValue > _sensorMax){
+      _sensorMax = _sensorValue;
+    }
+    if(_sensorValue < _sensorMin){
+      _sensorMin = _sensorValue;
+    }
+    Serial.println(_sensorMin);
+    Serial.println(_sensorMax);
+    delay(10);
+}
+
+void Irscalib::calibratingSensor()
+{
+  _sensorValue = analogRead(_sensorPin);
+  _sensorValue = map(_sensorValue, _sensorMin, _sensorMax, 180, 0); 
+  _sensorValue = constrain(_sensorValue, 0, 255);
+  Serial.println(_sensorValue);
+  delay(100);
+}
+
---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/Sharp_IR_Calibration/Irscalib.h/Irscalib.h.ino
@@ -0,0 +1,24 @@
+/*
+Practice Library Making (July 6, 2018)
+*/
+#ifndef Irscalib.h
+#define Irscalib.h
+
+#include "Arduino.h"
+class Irscalib
+{
+  public:
+    Irscalib(int sensorPin);
+    Irscalib(int sensorValue);
+    Irscalib(int sensorMin);
+    Irscalib(int sensorMax);
+    void adjustSensor();
+    void calibratingSensor();
+  private:
+    int _sensorPin;
+    int _sensorValue;
+    int _sensorMin;
+    int _sensorMax;
+};
+
+#endif
