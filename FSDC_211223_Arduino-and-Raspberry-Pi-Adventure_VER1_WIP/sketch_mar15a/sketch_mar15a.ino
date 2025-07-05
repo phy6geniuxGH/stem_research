287f8b15ba2a433e18f4ffa745d761aab39c04ab
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-07-06 00:33:38 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/sketch_mar15a/sketch_mar15a.ino
@@ -0,0 +1,16 @@
+// led is the givenname for digital pin 13.
+int led = 13;
+
+// the setting up of pins.
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+}
+
+// the loop is where your program runs repeatedly.
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED ON (HIGH)
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED OFF (LOW)
+  delay(1000);               // wait for a second
+}
