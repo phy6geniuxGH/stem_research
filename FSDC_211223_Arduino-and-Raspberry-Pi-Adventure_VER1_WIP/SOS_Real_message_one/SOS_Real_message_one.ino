8b02754d396fc63b8af132a5c28ee4fa64e67736
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-07-07 01:37:12 +0800

    Add files via upload

---
+++ FSDC_211223_Arduino-and-Raspberry-Pi-Adventure_VER1_WIP/SOS_Real_message_one/SOS_Real_message_one.ino
@@ -0,0 +1,52 @@
+#include <Morse.h>
+
+Morse morse(13);
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  morse.dot(); morse.dot(); morse.dot();
+  morse.dash(); morse.dash(); morse.dash();
+  morse.dot(); morse.dot(); morse.dot();
+  delay(3000);
+
+  morse.dot();morse.dot();morse.dot();morse.dot();
+  delay(100);morse.dot();morse.dot();delay(100);
+  delay(1000);delay(100);morse.dash();morse.dot();
+  morse.dash();morse.dot();delay(100);morse.dot();
+  morse.dash();morse.dot();delay(100);morse.dot();
+  morse.dot();morse.dash();delay(100);morse.dot();
+  morse.dot();morse.dot();delay(100);morse.dot();
+  morse.dot();morse.dot();morse.dot();
+}
+/*
+//Morse Code
+
+int pin = 13;
+
+void setup(){
+  pinMode(pin, OUTPUT);
+}
+void loop(){
+  dot(); dot(); dot();
+  dash(); dash(); dash();
+  dot(); dot(); dot();
+  delay(3000);
+}
+void dot(){
+  digitalWrite(pin, HIGH);
+  delay(250);
+  digitalWrite(pin, LOW);
+  delay(250);
+}
+void dash(){
+  digitalWrite(pin, HIGH);
+  delay(1000);
+  digitalWrite(pin, LOW);
+  delay(250);
+}
+*/
