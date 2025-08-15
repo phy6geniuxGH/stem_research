47949f4d73579e4834192e5a22dff30cf7b46125
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2025-08-16 07:25:48 +0800

    Add files via upload

---
+++ FSDC_211223_MATLAB-Projects_VER1_WIP/Computational Electromagnetics/OrigamiMatrices.m
@@ -0,0 +1,7 @@
+syms a1 a2 a3 a4;
+R1 = [cos(a1) -sin(a1) ; sin(a1) cos(a1)];
+R2 = [cos(a2) -sin(a2) ; sin(a2) cos(a2)];
+R3 = [cos(a3) -sin(a3) ; sin(a3) cos(a3)];
+R4 = [cos(a4) -sin(a4) ; sin(a4) cos(a4)];
+
+D = R1*R2*R3*R4;
